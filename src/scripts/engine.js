bd76545d69c58c90ea184972d86352abebe4c984
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2023-12-20 01:08:47 -0300

    Cerreção das viradas de cards.

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -17,18 +17,50 @@ const emojis = [
     "🦇"
 ];
 
-// Faz sorteio baseado em pesos
-let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
-
-// Desenhando os elementos
-
-for (let i = 0; i < emojis.length; i++){
-    let box = document.createElement("div");
-    box.className = "item";
-    box.innerHTML = shuffleEmojis[i];
-    document.querySelector(".game").appendChild(box);
-}
-
 let openCards = [];
 
+// Faz sorteio baseado em pesos
+let shuffeEmojis = emojis.sort(()=>(Math.random() > 0.5 ? 2 : -1));
 
+// Desenhando os elementos
+for(let i=0; i < emojis.length; i++){
+
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffeEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+
+}
+
+function handleClick(){
+
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if (openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+};
+
+// Verifica se os cards são iguais e se não vouver mais cards 
+function checkMatch(){
+
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }else{
+        // Se não forem iguais remove a classe e volta a se econder.
+        openCards[0].classList .remove("boxOpen");
+        openCards[1].classList .remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length===emojis.length){
+        alert("Voce ganhou!!!!")
+    }
+
+}
